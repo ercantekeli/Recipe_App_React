4f52e236b6f9c02b206473021ed0561db459e24e
ercantekeli <ercantekeli@outlook.com>
2022-04-05 23:00:49 +0300

    last commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./style";
-
-
-
-
+import styles from "./Navbar.module.css";
 const Navbar = () => {
+  const homeName = "<Ercan/>";
   return (
-    <div className="container">
-      <div className="left">
-        <Link to="/">Home</Link>
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <Link to="/">
+          <span>{homeName}</span>Recipe
+        </Link>
       </div>
 
-      <div className="right">
+      <div className={styles.right}>
         <Link to="/about">About</Link>
-        <a href="https://github.com/ercantekeli">Github</a>
-        <Link to="/login">Login</Link>
+        <a
+          href="https://github.com/ercantekeli"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Github
+        </a>
+        <Link to="/login">LogOut</Link>
       </div>
     </div>
   );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import styles from "./Login.module.css";
 
-const Login = () => {
+export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className={styles.container}>
+      <Stack
+        component="form"
+        sx={{
+          width: "25ch",
+        }}
+        spacing={2}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-small"
+          placeholder="USERNAME"
+          variant="filled"
+          size="normal"
+        />
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-normal"
+          placeholder="PASSWORD"
+          variant="filled"
+          type="password"
+        />
+        <Button variant="contained">LOGIN</Button>
+      </Stack>
+    </div>
+  );
 }
-
-export default Login
